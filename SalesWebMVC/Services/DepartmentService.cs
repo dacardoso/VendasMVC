4ce27f687ff5f5bb2df14ad11c571f5d7e4dfa18
Daniel Cardoso <da.eliseu@gmail.com>
2021-04-21 20:56:14 -0300

    Melhorando o View Seller e Departaments

---
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+using SalesWebMVC.Data;
+
+namespace SalesWebMVC.Services
+{
+    public class DepartmentService
+    {
+
+        private readonly SalesWebMVCContext _context;
+        public DepartmentService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departament> FindAll()
+        {
+            return _context.Departament.OrderBy(x=> x.Name).ToList();
+        }
+
+    }
+}
